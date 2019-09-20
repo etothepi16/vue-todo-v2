d9a6eaa05d7f7d860cc14ce613e7cb4f250dd515
etothepi16 <paulmcdowell18@gmail.com>
2019-09-20 12:48:09 -0400

    You are on this council, but we do not grant you the rank of master

---
+++ server/app/Controllers/Http/TaskController.js
@@ -0,0 +1,29 @@
+"use strict";
+const Task = use("App/Models/Task");
+const Project = use("App/Models/Project");
+const AuthorizationService = use("App/Services/AuthorizationService");
+
+class TaskController {
+  async index({ auth, request, params }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const project = await Project.find(id);
+    AuthorizationService.verifyPermission(project, user);
+    return await project.tasks().fetch();
+  }
+  async create({ request, params }) {
+    const user = await auth.getUser();
+    const { description } = request.all();
+    const { id } = params;
+    const project = await Project.find(id);
+    AuthorizationService.verifyPermission(project, user);
+    const task = new Task();
+    task.fill({
+      description
+    });
+    await project.tasks().save(task);
+    return task;
+  }
+}
+
+module.exports = TaskController;
